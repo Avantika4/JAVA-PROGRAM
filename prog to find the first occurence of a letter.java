55961c358e48454768c183d8a49b3d5729a807b2
Avantika Bhasin <68161426+Avantika4@users.noreply.github.com>
2020-07-13 20:49:53 +0530

    Create prog to find the first occurence of a letter.java

---
+++ prog to find the first occurence of a letter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args)
+    {
+      String s =" Burj Khailifa is the tallest building in world";
+     System.out.println(s.indexOf("i"));
+    }
+    }
